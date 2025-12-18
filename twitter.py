e1cafd77b8db92590a76cbac8077cccbe0f581d7
Mustafa Gelebek <gelebekmustafa3@gmail.com>
2025-12-19 02:32:03 +0300

    fix: variable names and ssl verify

--- twitter.py
+++ twitter.py
@@ -13,7 +13,7 @@ KONTROL_ARALIGI = 300
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
 # Twitter Bağlantısı
 def tweet_at(mesaj):
@@ -23,7 +23,7 @@ def tweet_at(mesaj):
             consumer_key=API_KEY,
             consumer_secret=API_SECRET,
             access_token=ACCESS_TOKEN,
-            access_token_secret=ACCESS_SECRET
+            access_token_secret=ACCESS_TOKEN_SECRET
         )
         client.create_tweet(text=mesaj)
         print("✅ Tweet başarıyla atıldı!")
